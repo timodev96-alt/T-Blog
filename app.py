e0a38a2283493aa05f3f6cdaa447a85b402ea90f
Timothy <timo.dev96@gmail.com>
2026-09-05 20:05:56 +0300

    added a create post feture

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask
+from flask import Flask, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -31,3 +31,28 @@ def show(post_id):
         <h1>{post['title']}</h1>
         <p>{post['body']}</p>
     """
+
+@app.route('/posts/create', methods=['GET','POST'])
+def create():
+    if request.method == 'POST':
+        conn = get_db()
+        title = request.form['title']
+        body = request.form['body']
+        conn.execute('INSERT INTO posts (title, body, author_id) VALUES (?,?,?)', (title, body, 1))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('index'))
+
+    return """
+        <form dir="ltr" action="/posts/create" method="post">
+        <label>Post Title:</label>
+        <br />
+        <input type="text" name="title">
+        <br />
+        <label>Post Body:</label>
+        <br />
+        <textarea name="body" cols="50" rows"10"></textarea>
+        <br />
+        <button type="submit">Create Post</button>
+    """
+    
